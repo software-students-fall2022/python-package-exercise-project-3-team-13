5a54343dc4b72142a2756d12a83f14ede45ef74b
EV9H <106489113+EV9H@users.noreply.github.com>
2022-11-07 13:26:48 -0500

    reformating: no global variables and added a "main" wrapper

--- src/team13LoremIpsum/penetrate.py
+++ src/team13LoremIpsum/penetrate.py
@@ -9,14 +9,8 @@ from rich.console import Console
 
 import hackertype
 
-# Global Var
-con = Console()
-bar = "|"+" "*10 + "|"
-progress = 0
-loading_bar_char = "█"
 
-
-def show_header():
+def show_header(con):
     banner = pyfiglet.figlet_format("DARKWEB", font="banner3-D")
     con.print(banner, "Penetrating server firewall in...", style="bold green")
     for i in range(0, 4):
@@ -27,57 +21,38 @@ def show_header():
             print(str(3-i) + "...", end='\r')
 
 
-def updatebar_by_input(increment_base=0.1):
-    global progress
-    global bar
+def updatebar(con):
+    bar = "|"+" "*10 + "|"
+    progress = 0
+    loading_bar_char = "█"
 
-    if(progress > 10):
-        os.system('cls')
-        print("SUCCESSFUL")
-
-    else:
-        increment = random.random() * increment_base
-        if math.floor(progress) < math.floor(progress + increment):
-            bar = "|" + math.floor(progress+increment) * loading_bar_char + \
-                (10-math.floor(progress+increment)) * "_" + "|"
-        progress += increment
-        # os.system('cls')
-        print(bar + "{:.2f}".format(progress*10) + "%", end='\r', flush=True)
-
-
-def updatebar():
-    global progress
-    global bar
-    global con
-    if(progress > 10):
-        print("                          ", end="\r")
-        con.print("SUCCESSFUL", style="bold green")
-        return False
-    else:
-        sleep(0.3)
-        increment = random.random()
-        if math.floor(progress) < math.floor(progress + increment):
-            bar = "[" + math.floor(progress+increment) * loading_bar_char + \
-                (10-math.floor(progress+increment)) * "_" + "]"
-        progress += increment
-        print("{:.2f}".format(min((progress*10), 100)
-                              ) + "%" + bar + str("{:.2f}".format(increment)) + "/s", end='\r', flush=True)
-
-        return True
+    while True:
+        if(progress > 10):
+            print("                          ", end="\r")
+            con.print("SUCCESSFUL", style="bold green")
+            break
+        else:
+            sleep(0.3)
+            increment = random.random()
+            if math.floor(progress) < math.floor(progress + increment):
+                bar = "[" + math.floor(progress+increment) * loading_bar_char + \
+                    (10-math.floor(progress+increment)) * "_" + "]"
+            progress += increment
+            print("{:.2f}".format(min((progress*10), 100)
+                                  ) + "%" + bar + str("{:.2f}".format(increment)) + "/s", end='\r', flush=True)
 
 
 def penetrate():
-    # keyboard.on_press(lambda e: updatebar_by_input())
-    # keyboard.wait('esc')
-    show_header()
-    while(updatebar()):
-        pass
+    con = Console()
+    show_header(con)
+    updatebar(con)
     con.print(
-        "Admin privileges acquired... Ready to implant virus", style="bold red")
+        "Admin privileges acquired... Ready to implant virus code", style="bold red")
     hackertype.hackertype('python')
     print("\n")
     con.print("\nLogged Out... Successfully implanted", style="bold green")
     sleep(1)
 
 
-penetrate()
+if __name__ == '__main__':
+    penetrate()
